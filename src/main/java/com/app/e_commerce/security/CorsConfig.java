7a03eb918fc3dd312fc655f4e6abbec5e0592c37
Tamilkumaran-dev <tamilkumaranofficial07@gmail.com>
2025-10-04 16:53:35 +0530

    change application property for db

--- src/main/java/com/app/e_commerce/security/CorsConfig.java
+++ src/main/java/com/app/e_commerce/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")            // allow all endpoints
-                        .allowedOrigins("http://localhost:5173") // frontend URL
+                        .allowedOrigins("*") // frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);      // allow cookies
